4e7665a0e0d13702c69e03eddd5d4480162efc53
Sahiljamil <sahiljamil97@googlemail.com>
2022-02-25 09:38:01 +0000

    Added movement and animations
    idle and movement animations added, jump animation needs looked at

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,37 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D body;
+    private Animator anim;
     [SerializeField] private float speed;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
     }
 
     private void Update()
     {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
         if (Input.GetKey(KeyCode.Space) ^ Input.GetKey(KeyCode.W))
         {
             body.velocity = new Vector2(body.velocity.x, speed);
         }
 
+        anim.SetBool("running", horizontalInput != 0);
     }
     
 
