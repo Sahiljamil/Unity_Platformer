79639bb21bfe7e43f17ea435172df5aa5fb89a54
Sahiljamil <sahiljamil97@googlemail.com>
2022-02-25 11:37:53 +0000

    Jumping Functionality
    Player restricted to single jump action, resized camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,12 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     [SerializeField] private float speed;
+    public bool isGrounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
+        isGrounded = true;
     }
 
     private void Update()
@@ -29,14 +31,22 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.Space) ^ Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space) ^ Input.GetKey(KeyCode.W) && isGrounded)
         {
             body.velocity = new Vector2(body.velocity.x, speed);
+            isGrounded = false;
         }
 
         anim.SetBool("running", horizontalInput != 0);
     }
-    
 
-   
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+
 }
